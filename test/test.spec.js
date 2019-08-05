60fd1fc5b13b7a1908bc308a0a4ed9e78b0dedc0
Yuriy Voziy <yuretsz@gmail.com>
2019-08-05 22:57:45 +0000

    TDD: First failing test

---
+++ test/test.spec.js
@@ -0,0 +1,25 @@
+const j = require('../src/index')
+const sinon = require('sinon')
+
+const uncarryThis = f => (...args) => f.apply(args)
+const reverse = str => str.split('').reverse().join('')
+const lower = uncarryThis(String.prototype.toLowerCase)
+const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1)
+const getName = obj => obj.name
+
+const log = sinon.spy();
+
+
+describe('Should getName->reverse->lower->capitalize->log', () => {
+  log(capitalize(lower(reverse(getName({ name: 'Buckethead' })))))
+  
+  it('should log Daehtekcub', function() {
+    sinon.assert.calledWith(log, 'Daehtekcub');   
+  })
+  
+  j ({name: 'Buckethead'}) `|>` (getName, reverse, lower, capitalize, log);
+  
+  it('should again log Daehtekcub', function() {
+    sinon.assert.calledWith(log, 'Daehtekcub');   
+  })
+})
